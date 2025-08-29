a9e88d3ba6de8a7fe62ee27a841a47c59faf8178
Tobak95 <akinola.tobi.2010@gmail.com>
2025-08-29 02:19:28 -0600

    hamburger

--- src/component/layout/NavBar.jsx
+++ src/component/layout/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { links } from "../../../data";
 import { Link } from "react-router-dom";
-
+import { IoMdMenu } from "react-icons/io";
+import { MdOutlineClose } from "react-icons/md";
+import { useState } from "react";
 const NavBar = () => {
+
+  const [showHamburger, setShowHamburger] = useState(false);
+
   return (
     <nav className="bg-black">
       <div className="layout px-4 text-[#C7C7C7]">
@@ -15,7 +20,7 @@ const NavBar = () => {
             </div>
           </Link>
 
-          <div className="flex justify-between items-center gap-3 lg:gap-6 lg:w-[207px] ">
+          <div className="hidden lg:flex justify-between items-center gap-3 lg:gap-6 lg:w-[207px] ">
             {links.map((link, index) => {
               return (
                 <a
@@ -29,6 +34,34 @@ const NavBar = () => {
               );
             })}
           </div>
+          {showHamburger ? (
+            <MdOutlineClose
+              className="text-[30px] lg:hidden cursor-pointer "
+              onClick={() => setShowHamburger(!showHamburger)}
+            />
+          ) : (
+            <IoMdMenu
+              className="text-[30px] lg:hidden cursor-pointer"
+              onClick={() => setShowHamburger(!showHamburger)}
+            />
+          )}
+
+          {showHamburger && (
+            <div className="absolute top-[80px] right-0 rounded-xl border border-[#D3E97A] bg-black flex flex-col items-center gap-6 py-6 px-10 text-white lg:hidden z-10">
+              {links.map((link, index) => {
+                return (
+                  <a
+                    style={{ fontFamily: "Inter,sans-serif" }}
+                    key={index}
+                    href={link.to}
+                    className="font-[Inter]"
+                  >
+                    {link.title}
+                  </a>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </nav>
